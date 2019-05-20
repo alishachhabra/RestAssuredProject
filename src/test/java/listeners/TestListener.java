c06f8b8f94d56377343dbaf58815f9fcac2219a8
Alisha Parmar <alisha.parmar@infostretch.com>
2019-05-20 17:53:55 +0530

    updated

--- src/test/java/listeners/TestListener.java
+++ src/test/java/listeners/TestListener.java
@@ -2,9 +2,23 @@ package listeners;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.sun.xml.bind.Util;
 
 import test.RestTest2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
 import org.testng.IInvokedMethod;
 import org.testng.IInvokedMethodListener;
 import org.testng.ISuite;
@@ -56,8 +70,73 @@ public class TestListener extends RestTest2 implements ITestListener, ISuiteList
 	public synchronized void onTestFailure(ITestResult result) {
 		System.out.println((result.getMethod().getMethodName() + " failed!"));
 		test.get().fail(result.getThrowable());
-	}
+		
+		test.get().info("*** Test execution " + result.getMethod().getMethodName() + " failed...");
+		test.get().info((result.getMethod().getMethodName() + " failed!"));
+		
+		ITestContext context = result.getTestContext();
+		WebDriver driver = (WebDriver) context.getAttribute("driver");
+		
+		String targetLocation = null;
 
+		String testClassName = result.getInstanceName().trim();
+		Date now = new java.util.Date();
+		Timestamp current = new java.sql.Timestamp(now.getTime());
+		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(current);
+		//String timeStamp = Util.getCurrentTimeStamp(); // get timestamp
+		String testMethodName = result.getName().toString().trim();
+		String screenShotName = testMethodName + timeStamp + ".png";
+		//String fileSeperator = System.getProperty("file.separator");
+		String reportsPath = System.getProperty("user.dir") + "\\" + "TestReport" + "\\"
+				+ "screenshots";
+		
+		test.get().info("Screen shots reports path - " + reportsPath);
+	
+
+		try {
+			File file = new File(reportsPath + "\\" + testClassName); // Set
+																				// screenshots
+																				// folder
+			if (!file.exists()) {
+				if (file.mkdirs()) {
+					test.get().info("Directory: " + file.getAbsolutePath() + " is created!");
+				} else {
+					test.get().info("Failed to create directory: " + file.getAbsolutePath());
+				}
+
+			}
+
+			targetLocation = reportsPath + "\\" + testClassName + "\\" + screenShotName;
+			
+			
+			File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			// define
+				
+			// location
+						File targetFile = new File(targetLocation);
+						test.get().info("Screen shot file location - " + screenshotFile.getAbsolutePath());
+						test.get().info("Target File location - " + targetFile.getAbsolutePath());
+						FileHandler.copy(screenshotFile, targetFile);
+
+					} catch (FileNotFoundException e) {
+						test.get().info("File not found exception occurred while taking screenshot " + e.getMessage());
+					} catch (Exception e) {
+						test.get().info("An exception occurred while taking screenshot " + e.getCause());
+					}
+
+					// attach screenshots to report
+					try {
+						test.get().fail("Screenshot",
+								MediaEntityBuilder.createScreenCaptureFromPath(targetLocation).build());
+					} catch (IOException e) {
+						test.get().info("An exception occured while taking screenshot " + e.getCause());
+					}
+					test.get().log(Status.FAIL, "Test Failed");
+				}
+		
+
+
+	
 	@Override
 	public synchronized void onTestSkipped(ITestResult result) {
 		System.out.println((result.getMethod().getMethodName() + " skipped!"));
